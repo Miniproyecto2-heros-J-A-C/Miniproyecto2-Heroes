599a701eaa189c4a916cba7a79ac825edd9d11da
Damian Gallegos <damiangallegos010@gmail.com>
2022-05-15 16:54:50 -0500

    Tabla commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,22 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  indice: number = 0;
+  mail: string[] = [];
+  name: string[] = [];
+  sex: string[] = [];
+  date: string[] = [];
 
-
-storeName(email:any,nombre:any,sexo:any,fecha:any) {
-  localStorage.setItem('email',email);
-  localStorage.setItem('nombre',nombre);
-  localStorage.setItem('sexo',sexo);
-  localStorage.setItem('fecha',fecha);
-}
-
-  constructor() { }
-
-  ngOnInit(): void {
+  storeName(email: any, nombre: any, sexo: any, fecha: any) {
+    this.mail[this.indice] = email;
+    this.name[this.indice] = nombre;
+    this.sex[this.indice] = sexo;
+    this.date[this.indice] = fecha;
+    localStorage.setItem('email', JSON.stringify(this.mail));
+    localStorage.setItem('nombre', JSON.stringify(this.name));
+    localStorage.setItem('sexo', JSON.stringify(this.sex));
+    localStorage.setItem('fecha', JSON.stringify(this.date));
+    this.indice++;
+    console.log(this.indice);
+    console.log(this.mail);
   }
 
-}
+  constructor() {}
 
+  ngOnInit(): void {}
+}
--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -4,27 +4,23 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
-  styleUrls: ['./tabla.component.css']
+  styleUrls: ['./tabla.component.css'],
 })
 export class TablaComponent implements OnInit {
- 
-  
-  nombre:any;
-  sexo:any;
-  fecha:any;
-  email:any;
-  constructor(){
-      
-      this.obtenerdatos();
-  }
-  obtenerdatos(){
-    this.nombre = localStorage.getItem('nombre');
-    this.email = localStorage.getItem('email');
-    this.sexo = localStorage.getItem('sexo');
-    this.fecha = localStorage.getItem('fecha');
-   
-  }
-  ngOnInit(): void {
-  }
+  nombre: any[] = [];
+  sexo: any[] = [];
+  fecha: any;
+  email: any;
+  indice: number = 0;
 
+  constructor() {
+    this.obtenerdatos();
+  }
+  obtenerdatos() {
+    this.nombre = JSON.parse(localStorage.getItem('nombre') || '{}');
+    this.email = JSON.parse(localStorage.getItem('email') || '{}');
+    this.sexo = JSON.parse(localStorage.getItem('sexo') || '{}');
+    this.fecha = JSON.parse(localStorage.getItem('fecha') || '{}');
+  }
+  ngOnInit(): void {}
 }
