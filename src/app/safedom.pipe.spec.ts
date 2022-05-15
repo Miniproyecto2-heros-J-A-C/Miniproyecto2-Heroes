23775c1e26123025023db94bb77d891c8061c975
Damian Gallegos <damiangallegos010@gmail.com>
2022-05-15 01:21:09 -0500

    Auto-generated commit [ci skip]

--- src/app/safedom.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SafedomPipe } from './safedom.pipe';
-
-describe('SafedomPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafedomPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
