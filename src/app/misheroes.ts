9463e50a9eae8cfe7431e0fa4302f4b04cb8402b
cesarini1234 <102565859+cesarini1234@users.noreply.github.com>
2022-05-15 11:14:01 -0500

    Update misheroes.ts

--- src/app/misheroes.ts
+++ src/app/misheroes.ts
@@ -1,52 +1,94 @@
 import { Heroe } from "./heroe";
 export const HEROES:Heroe[]=[
-    {
-        nombre: "Aquaman",
-        bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
-        img: "assets/img/aquaman.png",
-        aparicion: "1941-11-01",
-        casa:"DC"
-      },
-      {
-        nombre: "Batman",
-        bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
-        img: "assets/img/batman.png",
-        aparicion: "1939-05-01",
-        casa:"DC"
-      },
-      {
-        nombre: "Daredevil",
-        bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
-        img: "assets/img/daredevil.png",
-        aparicion: "1964-01-01",
-        casa: "Marvel"
-      },
-      {
-        nombre: "Hulk",
-        bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
-        img: "assets/img/hulk.png",
-        aparicion: "1962-05-01",
-        casa:"Marvel"
-      },
-      {
-        nombre: "Linterna Verde",
-        bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)",
-        img: "assets/img/linterna-verde.png",
-        aparicion: "1940-06-01",
-        casa: "DC"
-      },
-      {
-        nombre: "Spider-Man",
-        bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
-        img: "assets/img/spiderman.png",
-        aparicion: "1962-08-01",
-        casa: "Marvel"
-      },
-      {
-        nombre: "Wolverine",
-        bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
-        img: "assets/img/wolverine.png",
-        aparicion: "1974-11-01",
-        casa: "Marvel"
-      }
+  {
+    nombre: "Aquaman",
+    bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+    img: "assets/img/aquaman.png",
+    aparicion: "1941-11-01",
+    casa:"DC"
+  },
+  {
+    nombre: "Batman",
+    bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+    img: "assets/img/batman.png",
+    aparicion: "1939-05-01",
+    casa:"DC"
+  },
+  {
+    nombre: "Daredevil",
+    bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
+    img: "assets/img/daredevil.png",
+    aparicion: "1964-01-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Hulk",
+    bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
+    img: "assets/img/hulk.png",
+    aparicion: "1962-05-01",
+    casa:"Marvel"
+  },
+  {
+    nombre: "Linterna Verde",
+    bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)",
+    img: "assets/img/linterna-verde.png",
+    aparicion: "1940-06-01",
+    casa: "DC"
+  },
+  {
+    nombre: "Spider-Man",
+    bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
+    img: "assets/img/spiderman.png",
+    aparicion: "1962-08-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Wolverine",
+    bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
+    img: "assets/img/wolverine.png",
+    aparicion: "1974-11-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Thor",
+    bio: "Thor es el hijo de Odín, señor de los dioses de Asgard, y de Jord, que también es conocida como Gea, la más anciana diosa de la Tierra. Odín quería ser padre de un hijo cuyo poder derivase de la Tierra y de Asgard, así que se apareó con Jord. Odín creó una cueva en Noruega donde Jord dio a luz a Thor",
+    img: "assets/img/thor.png",
+    aparicion: "1962-08-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Deadpool",
+    bio: "Antihéroe de Marvel, que se caracteriza por su humor irreverente, superpoderes varios que emplea a favor de luchar contra los malos. Se trata de un superhéroe bastante original, básicamente porque no lo es sino más bien es un antihéroe, dueño de un humor irreverente que precisamente podemos citar sin dudas como la clave del fanatismo que ha despertado entre sus seguidores.",
+    img: "assets/img/dead.png",
+    aparicion: "1991-02-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Flash",
+    bio: "Jay Garrick era un estudiante universitario en 1938 que accidentalmente inhaló vapores de agua pesada después de tomar un descanso para fumar dentro de su laboratorio donde había estado trabajando.18​ Como resultado, descubrió que podía correr a una velocidad sobrehumana y que tenía reflejos igualmente rápidos",
+    img: "assets/img/flash.png",
+    aparicion: "1956-05-01",
+    casa: "DC"
+  },
+  {
+    nombre: "Iron-Man",
+    bio: "Anthony Edward Tony Stark fue un multimillonario industrial, anterior Director General de Industrias Stark y miembro fundador de los Vengadores. Siendo el hijo de Howard Stark y poseyendo un gran intelecto, Stark se volvió un inventor de armas mundialmente reconocido hasta que fue secuestrado por los Diez Anillos.",
+    img: "assets/img/ironman.png",
+    aparicion: "1968-05-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Black-Widow",
+    bio: "fue una superheroína ficticia que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por el editor y trazador Stan Lee, el guionista Don Rico y el artista Don Heck, el personaje debutó en Tales of Suspense #52 (abril de 1964). El personaje fue presentado como una espía rusa, una antagonista del superhéroe Iron Man. Más tarde, desertó a los Estados Unidos, convirtiéndose en una agente de la organización ficticia S.H.I.E.L.D., y miembro del equipo de superhéroes Los Vengadores",
+    img: "assets/img/black.png",
+    aparicion: "1964-04-01",
+    casa: "Marvel"
+  },
+  {
+    nombre: "Ant-Man",
+    bio: "Creado por David Michelinie y John Byrne, Scott Lang apareció por primera vez en The Avengers # 181 (marzo de 1979) y en Marvel Premiere # 47 (abril de 1979) como el segundo personaje de superhéroes en usar el nombre de Ant-Man en el Universo Marvel. Es un ladrón reformado y un experto en electrónica. Fue miembro de Los Vengadores, un personaje destacado en la serie de cómics FF, y en 2015 se convirtió en el personaje principal de la serie Ant-Man.",
+    img: "assets/img/ant.png",
+    aparicion: "1979-03-01",
+    casa: "Marvel"
+  }
 ];
