2d7dce11a006a3e088fd4efe06955a662567f67e
XvXFenixLopezXvX <102643115+XvXFenixLopezXvX@users.noreply.github.com>
2022-05-15 12:19:33 -0500

    Add files via upload

---
+++ src/app/sugerencias/sugerencias.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Heroe } from '../heroe';
+import { HeroeService } from '../shared/heroe.service';
+@Component({
+  selector: 'app-sugerencias',
+  templateUrl: './sugerencias.component.html',
+  styleUrls: ['./sugerencias.component.css']
+})
+export class SugerenciasComponent implements OnInit {
+  i: string=''
+  miheroe: Heroe={
+    nombre:"",
+    bio:"",
+    img:"",
+    aparicion:"",
+    casa:""
+  }
+
+  constructor( private heroeService: HeroeService, private activatedRoute: ActivatedRoute) {
+    this.miheroe = this.heroeService.getUnHeroe(1);
+  }
+
+
+  ngOnInit(): void {
+    
+
+  }
+
+}
